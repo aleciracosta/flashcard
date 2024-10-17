9e3b306a3f5f57eea9bdebf0c0a8ba476758c7fc
aleciracosta <167779380+aleciracosta@users.noreply.github.com>
2024-10-17 07:36:14 -0300

    77787878787878

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criacartao{
+    'progrmação',
+    'o oque é pytho?',
+    'o python é uma linguagem de programação'
+}
+
+criacartao{
+    'Geografia'
+    'Qual a capital da frança?',
+    'A capital da frança é Paris'
+}
